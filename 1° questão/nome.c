ed009a1351db505207e9468929ac5f39681eca06
joaodelf <151640247+joaodelf@users.noreply.github.com>
2023-11-21 16:37:01 -0300

    implementação da função buscar
    Foi utilizado o string P para receber a informação que qual elemento buscar  comparando com o string Nome, assim fazendo com que encontre o valor que está sendo buscado pelo usuário. Para a comparação, já que os no string não é utilizado operadores, foi feito o uso da função strcmp na biblioteca string.h, assim se os dois valores forem verdadeiros ( igual a 0) retorna o valor de P.

--- 1° questão/nome.c
+++ 1° questão/nome.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "nome.h"
+#include <string.h>
 
 Lista* lst_cria();
 
@@ -8,7 +9,15 @@ Lista* lst_insere(Lista* l, char nome);
 
 Lista* lst_remove(Lista* l, char nome);
 
-Lista* lst_buscar(Lista* l, char nome);
+Lista* lst_buscar(Lista* l, char nome) {
+	Lista* p;
+	for(p=l; p!=NULL; p = p->prox){
+		if(strcmp(p->info, nome) == 0)
+			return p;
+	
+	}
+	return NULL;
+};
 
 void lst_imprimi(Lista* l);
 
